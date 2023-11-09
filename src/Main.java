cbea1a994286da852aee532e5723068dec02f9d2
dashoes15 <daltonasali@gmail.com>
2023-11-09 09:40:59 -0500

    perfect squares choice proj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int number;
+        System.out.println("enter a number. i will tell you if it is a perfect square");
+        if (scan.hasNextInt()) {
+            number = scan.nextInt();
+            if (Math.sqrt(number)%1 == 0) {
+                System.out.println("your number is a perfect square");
+            }else{
+                System.out.println("not a perfect square");
+            }
+        }else{
+            System.out.println("you've entered something invalid.");
+        }
+
     }
 }
